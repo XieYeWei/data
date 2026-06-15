6dba582b2f8e5ad3c9bcfd437a92199c128481e0
XieYeWei <62280466+XieYeWei@users.noreply.github.com>
2026-06-15 17:42:33 +0800

    Add UserMapper with MyBatis-Plus BaseMapper

---
+++ hermes-backend/src/main/java/com/hermes/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.hermes.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hermes.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
