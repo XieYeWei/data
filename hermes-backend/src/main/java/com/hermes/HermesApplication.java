de127a6e355458f27e843870c0e0b924728222c3
XieYeWei <62280466+XieYeWei@users.noreply.github.com>
2026-06-15 17:48:09 +0800

    Enable @EnableScheduling for metrics collection task

--- hermes-backend/src/main/java/com/hermes/HermesApplication.java
+++ hermes-backend/src/main/java/com/hermes/HermesApplication.java
@@ -2,12 +2,14 @@ package com.hermes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Enterprise Hadoop Cluster Management Platform - Backend
- * Based on official Hadoop 3.3.x client libraries for HDFS/YARN/MapReduce
+ * Based on official Hadoop 3.3.x client libraries
  */
 @SpringBootApplication
+@EnableScheduling
 public class HermesApplication {
 
     public static void main(String[] args) {
