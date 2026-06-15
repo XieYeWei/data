52a68a21339b54306e9b0437904f1f4b5ef8ff15
XieYeWei <62280466+XieYeWei@users.noreply.github.com>
2026-06-15 17:42:35 +0800

    Add ClusterMapper

---
+++ hermes-backend/src/main/java/com/hermes/mapper/ClusterMapper.java
@@ -0,0 +1,9 @@
+package com.hermes.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hermes.entity.Cluster;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ClusterMapper extends BaseMapper<Cluster> {
+}
