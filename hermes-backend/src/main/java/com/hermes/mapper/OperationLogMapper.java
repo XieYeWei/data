f5f2c002e43a2428639e545f2517f149c1047db8
XieYeWei <62280466+XieYeWei@users.noreply.github.com>
2026-06-15 17:42:38 +0800

    Add OperationLogMapper for audit

---
+++ hermes-backend/src/main/java/com/hermes/mapper/OperationLogMapper.java
@@ -0,0 +1,9 @@
+package com.hermes.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hermes.entity.OperationLog;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OperationLogMapper extends BaseMapper<OperationLog> {
+}
