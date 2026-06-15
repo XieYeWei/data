d15ad5f179c6d3c47f64b18a902fa6fb8ecaeffc
XieYeWei <62280466+XieYeWei@users.noreply.github.com>
2026-06-15 18:01:47 +0800

    Add QueueAlertRuleMapper

---
+++ hermes-backend/src/main/java/com/hermes/mapper/QueueAlertRuleMapper.java
@@ -0,0 +1,9 @@
+package com.hermes.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hermes.entity.QueueAlertRule;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QueueAlertRuleMapper extends BaseMapper<QueueAlertRule> {
+}
